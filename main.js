0e3364b0f0b6c251683b563a5553ccb237b25d7e
lanmower <almagestfraternite@gmail.com>
2021-08-17 15:41:17 +0200

    first commit

--- main.js
+++ main.js
@@ -84,6 +84,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
+  DHT.destroy();
   if (process.platform !== 'darwin') app.quit()
 })
 
